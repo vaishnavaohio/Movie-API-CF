780fd880d06c9f4f81ac9b157d19224149929919
Acharyaseva <46236758+Acharyaseva@users.noreply.github.com>
2021-02-17 21:22:39 -0500

    Task 2.4
    If a commit is made edits for task 2.4 were made.

---
+++ index.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+const express = require("express");
+morgan = require("morgan");
+const app = express();
+
+app.use(morgan("common"));
+
+app.use(express.static("public"));
+
+// middleware to detect errors
+app.use((err, req, next) => {
+    console.error(err.stack);
+    response.status(500).send("Something broke!");
+});
+
+// get requests
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to my movie app!")
+});
+
+app.get("/movies", (req, res) => {
+    res.status(200).send(movies);
+})
+
+app.get("/documentation", (req, res) =>{
+    res.sendFile("public/documentation.html",{root: __dirname});
+});
+
+app.listen(8080, () => {
+    console.log("your app is listening on port 8080");
+});
