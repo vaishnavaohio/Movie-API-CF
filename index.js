4cc39f5a0492dc4ee8a251d1ab07f49e2544eda7
Acharyaseva <46236758+Acharyaseva@users.noreply.github.com>
2021-02-18 19:27:24 -0500

    Task 2.4
    If a commit is made revisions for task 2.4 were made.

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-const { response } = require("express");
 const express = require("express");
-morgan = require("morgan");
+const morgan = require('morgan');
+
 const app = express();
 
-app.use(morgan("common"));
-
-app.use(express.static("public"));
-
-// middleware to detect errors
-app.use((err, req, next) => {
-    console.error(err.stack);
-    response.status(500).send("Something broke!");
-});
-
-// get requests
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to my movie app!")
-});
-
-app.get("/movies", (req, res) => {
-    res.status(200).send(movies);
-})
-
-app.get("/documentation", (req, res) =>{
-    res.sendFile("public/documentation.html",{root: __dirname});
-});
-
-app.listen(8080, () => {
-    console.log("your app is listening on port 8080");
-});
-
-// object variable with top movies
-
-
-let movies = [
-  {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-  }
+let topMovies = [{
+        title: 'Gone with the Wind',
+        stars: ['Clark Gable', 'Vivien Leigh'],
+        director: 'Victor Fleming',
+        year: 1939,
+        rating: 8.1
+    },
+    {
+        title: 'The Sound of Music',
+        stars: ['Julie Andrews', 'Christopher Plummer'],
+        director: 'Robert Wise',
+        year: 1965,
+        rating: 8.0
+    },
+    {
+        title: 'Black Panther',
+        stars: ['Chadwick Boseman', 'Michael B. Jordan'],
+        director: 'Ryan Coogler',
+        year: 2018,
+        rating: 7.3
+    },
+    {
+        title: 'Pride & Prejudice',
+        stars: ['Keira Knightley', 'Matthew Macfadyen'],
+        director: 'Joe Wright',
+        year: 2005,
+        rating: 7.8
+    },
 ];
+
+app.use(morgan('common'));
+
+app.use('/documentation', express.static('public'));
+
+//get request
+app.get('/', (req, res) => {
+    res.send('Welcome to my movie index');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.use(express.static('public'));
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
